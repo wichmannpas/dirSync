854ce871b6266291b8137c28b0ebcde4e96d442b
Pascal Wichmann <wichmannpas@gmail.com>
2015-09-07 19:10:13 +0200

    configuration files used

--- dirSync.sh
+++ dirSync.sh
@@ -2,11 +2,12 @@
 # This script watches a directory for changes and uploads them to a remote host via rsync
 # Copyright (c) 2015 Pascal Wichmann
 
-dir="/dir/to/be/watched/"
-remote="user@host:/path/to/remote/dir/"
+configDir="/home/$(echo $USER)/.dirSync/"
 
+# try to load configuration
+source $configDir/$1
 
 inotifywait -r -m $dir -e moved_to -e modify -e attrib |
     while read path action file; do
-	rsync -r $dir $remote
+	rsync -r $rsyncParams $dir $remote
     done
